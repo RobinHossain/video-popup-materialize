71114073a7b265ce6e39f6b2bb2319ca1810359c
Robin Hossain <robinsabbir@gmail.com>
2016-12-12 18:37:10 +0600

    Source UPdated

--- js/init.js
+++ js/init.js
@@ -13,10 +13,22 @@
     })
 
     $('.open-video-popup').on('click', function () {
-      var getUrl = $(this).attr("data-url");
-      $('#videoUrl').attr('src', getUrl)
-      $('#videoPlayerPopup').addClass('is-visible');
+      var getVideoUrl = $(this).attr("video-url");
+      var getPosterUrl = $(this).attr("src");
+      // $('#videoUrl').attr('src', getVideoUrl)
+
       players = plyr.setup(document.querySelector('.sochara-player'), [{autoplay: false, hideControls: true}]);
+      players[0].source({
+        type: 'video',
+        sources: [{
+          src:  getVideoUrl,
+          type: 'video/mp4'
+        }],
+        poster: getPosterUrl
+      });
+
+      $('#videoPlayerPopup').addClass('is-visible');
+
     })
     // Working With Video Popup
 
